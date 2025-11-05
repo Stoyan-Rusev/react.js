0ee96efb5ade39339aaa27d19cb3d18e4db4ddfe
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-11-05 17:00:49 +0200

    add: UncontrolledForm

---
+++ 05.react-forms/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import UncontrolledForm from './components/UncontrolledForm.JSX'
+
+function App() {
+  return (
+    <>
+      <UncontrolledForm />
+    </>
+  )
+}
+
+export default App
---
+++ 05.react-forms/src/components/UncontrolledForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+export default function UncontrolledForm() {
+    const [pending, setPending] = useState(false);
+
+    const fakeFetch = (data) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({
+                    status: 200,
+                    message: 'success',
+                    data: data,
+                });
+            }, 1500);
+        });
+    };
+
+    const formSubmit = async (e) => {
+        e.preventDefault();
+        setPending(true);
+
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+
+        const data = Object.fromEntries(formData.entries());
+        const result = await fakeFetch(data);
+
+        setPending(false);
+        console.log(result);
+    };
+
+    return (
+        <>
+            <h2>Uncontrolled Form</h2>
+
+            <form onSubmit={formSubmit}>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div>
+                    <input type="submit" value="Submit" disabled={pending}/>
+                </div>
+            </form>
+
+        </>
+    );
+};
