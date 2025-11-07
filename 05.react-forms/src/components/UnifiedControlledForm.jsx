027c49c672b498c7dd4e7a2bbbdb7db239163caa
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-11-07 11:15:56 +0200

    add: UnifiedControlledForm

---
+++ 05.react-forms/src/components/UnifiedControlledForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import fakeFetch from "../services/userService";
+
+export default function UnifiedControlledForm() {
+    const [pending, setPending] = useState(false);
+    const [formData, setFormData] = useState({ username: '', password: '' });
+
+    const changeFormData = (e) => {
+        setFormData(prevData => ({ 
+            ...prevData,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        setPending(true);
+
+        const result = await(fakeFetch(formData));
+        setPending(false);
+    };
+
+    return (
+        <>
+            <h2>Unified Controlled Form</h2>
+
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" value={formData.username} onChange={changeFormData} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" value={formData.password} onChange={changeFormData} />
+                </div>
+                <input type="submit" value="Submit" disabled={pending} />
+            </form>
+        </>
+    );
+};
