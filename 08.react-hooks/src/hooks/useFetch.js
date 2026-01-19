56d66332b29cd98ff2e2c85e80dc1e9db849e0f7
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2026-01-19 23:55:18 +0200

    fix: useFetch logic

--- 08.react-hooks/src/hooks/useFetch.js
+++ 08.react-hooks/src/hooks/useFetch.js
@@ -10,10 +10,10 @@ export function useFetch(url) {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                setState(data);
-                setPending(false);
+                setState(Object.values(data));
             })
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
+            .finally(() => setPending(false));
     }, [url]);
 
     return [pending, state];
